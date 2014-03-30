66623047ecab56dee1c3b992a7bc1bd9bafed708
Babis Karypidis <xabikos@hotmail.com>
2014-03-30 23:23:04 +0200

    Revert model

--- WorldCup/Models/IdentityModels.cs
+++ WorldCup/Models/IdentityModels.cs
@@ -13,7 +13,6 @@ namespace WorldCup.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName1 { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 

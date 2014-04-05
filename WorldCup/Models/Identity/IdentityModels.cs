5be02ee63412a3038b544314fbb38ffb3cae15d3
Babis Karypidis <xabikos@hotmail.com>
2014-04-05 22:17:26 +0200

    Just to drop database in Azure

--- WorldCup/Models/Identity/IdentityModels.cs
+++ WorldCup/Models/Identity/IdentityModels.cs
@@ -14,6 +14,7 @@ namespace WorldCup.Models.Identity
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Dummy { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 

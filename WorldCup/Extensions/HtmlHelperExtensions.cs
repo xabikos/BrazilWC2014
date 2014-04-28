a33a8c47597d900fd3d6d0b4a4b388afaf52318c
Babis Karypidis <xaralaboskaripidis@gmail.com>
2014-04-28 14:45:15 +0200

    Add extension method for bootstrap editor

---
+++ WorldCup/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace WorldCup.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Helper method that adds the required class for bootstrap form controls
+        /// </summary>
+        public static MvcHtmlString BootstrapEditorFor<TModel, TValue>(this HtmlHelper<TModel> html,
+            Expression<Func<TModel, TValue>> expression)
+        {
+            return html.EditorFor(expression, new {htmlAttributes = new {@class = "form-control"}});
+        }
+
+    }
+}

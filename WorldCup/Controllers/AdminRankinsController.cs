c3c5d35697e5c96b889fc139c0f55650d4c929f2
Babis Karypidis <xabikos@hotmail.com>
2014-05-10 19:48:27 +0200

    Calculate match points per user

---
+++ WorldCup/Controllers/AdminRankinsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WorldCup.Common.Entities;
+using WorldCup.Extensions;
+
+namespace WorldCup.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminRankinsController : ControllerBase
+    {
+        // GET: AdminRankins
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> UpdateMatchRankings()
+        {
+            var systemParameters = await Context.SystemParameters.FirstOrDefaultAsync();
+
+            if(systemParameters == null)
+                return View("Index", model: "Initialize the system parameters and then update the rankings");
+
+            foreach (var match in Context.Matches.Where(m=>m.State == MatchState.Finalized))
+            {
+                foreach (var user in UserManager.AllUsers)
+                {
+                    // The calculation has be done already
+                    if (user.MatchPoints.Any(mp => mp.MatchId == match.Id)) 
+                        continue;
+
+                    var matchPoints = CalculateMatchPoints(user, match, systemParameters);
+                    user.MatchPoints.Add(new MatchPoints
+                    {
+                        MatchId = match.Id,
+                        Points = matchPoints
+                    });
+                }
+            }
+
+            await Context.SaveChangesAsync();
+            return View("Index", model: "You successfully update all match rankings");
+        }
+
+        public async Task<ActionResult> UpdateLongRunningRankings()
+        {
+            var systemParameters = await Context.SystemParameters.FirstOrDefaultAsync();
+
+            if (systemParameters == null)
+                return View("Index", model: "Initialize the system parameters and then update the rankings");
+
+            return View("Index", model: "You successfully update long running rankings");
+        }
+
+        private static int CalculateMatchPoints(ApplicationUser user, Match match, SystemParameters systemParameters)
+        {
+            // Find the user's prediction for this match if exists
+            var userPrediction = user.MatchPredictions.FirstOrDefault(mp => mp.MatchId == match.Id);
+            // If the user has not done prediction for this match return 0
+            if (userPrediction == null)
+            {
+                return 0;
+            }
+
+            return match.IsGroupStage()
+                ? CalculateGroupMatchResult(match, systemParameters, userPrediction)
+                : CalculateStageMatchResult(match, systemParameters, userPrediction);
+        }
+
+        private static int CalculateGroupMatchResult(Match match, SystemParameters systemParameters,
+            MatchPrediction userPrediction)
+        {
+            var result = 0;
+
+            //Half time score
+            if (userPrediction.HomeTeamHalfTimeGoals == match.HomeTeamHalfTimeGoals &&
+                userPrediction.AwayTeamHalfTimeGoals == match.AwayTeamHalfTimeGoals)
+            {
+                result += systemParameters.GroupHalfTimeScoreFactor;
+            }
+
+            //Full time score
+            if (userPrediction.HomeTeamFullTimeGoals == match.HomeTeamFullTimeGoals &&
+                userPrediction.AwayTeamFullTimeGoals == match.AwayTeamFullTimeGoals)
+            {
+                result += systemParameters.GroupFullTimeScoreFactor;
+            }
+
+            // Winner
+            if (userPrediction.Result == match.Result)
+            {
+                result += systemParameters.GroupWinnerFactor;
+            }
+
+            // YellowCards
+            if (userPrediction.YellowCards == match.YellowCards)
+            {
+                result += systemParameters.GroupYellowCardsNumberFactor;
+            }
+
+            // RedCards
+            if (userPrediction.RedCards == match.RedCards)
+            {
+                result += systemParameters.GroupRedCardsNumberFactor;
+            }
+
+            return result;
+        }
+
+        private static int CalculateStageMatchResult(Match match, SystemParameters systemParameters, MatchPrediction userPrediction)
+        {
+            var result = 0;
+
+            //Half time score
+            if (userPrediction.HomeTeamHalfTimeGoals == match.HomeTeamHalfTimeGoals &&
+                userPrediction.AwayTeamHalfTimeGoals == match.AwayTeamHalfTimeGoals)
+            {
+                result += systemParameters.FinalsHalfTimeScoreFactor;
+            }
+
+            //Full time score
+            if (userPrediction.HomeTeamFullTimeGoals == match.HomeTeamFullTimeGoals &&
+                userPrediction.AwayTeamFullTimeGoals == match.AwayTeamFullTimeGoals)
+            {
+                result += systemParameters.FinalsFullTimeScoreFactor;
+            }
+
+            // Winner
+            if (userPrediction.Result == match.Result)
+            {
+                result += systemParameters.FinalsWinnerFactor;
+            }
+
+            // YellowCards
+            if (userPrediction.YellowCards == match.YellowCards)
+            {
+                result += systemParameters.FinalsYellowCardsNumberFactor;
+            }
+
+            // RedCards
+            if (userPrediction.RedCards == match.RedCards)
+            {
+                result += systemParameters.FinalsRedCardsNumberFactor;
+            }
+
+            return result;
+        }
+        
+    }
+}
---
+++ WorldCup/Extensions/MatchExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WorldCup.Common.Entities;
+
+namespace WorldCup.Extensions
+{
+    /// <summary>
+    /// Contains extension methods for Match
+    /// </summary>
+    public static class MatchExtensions
+    {
+        /// <summary>
+        /// Indicates if a Group or next round match
+        /// </summary>
+        public static bool IsGroupStage(this Match match)
+        {
+            return match.Stage == MatchStage.GroupA ||
+                   match.Stage == MatchStage.GroupB ||
+                   match.Stage == MatchStage.GroupC ||
+                   match.Stage == MatchStage.GroupD ||
+                   match.Stage == MatchStage.GroupE ||
+                   match.Stage == MatchStage.GroupF ||
+                   match.Stage == MatchStage.GroupG ||
+                   match.Stage == MatchStage.GroupH;
+        }
+
+    }
+}

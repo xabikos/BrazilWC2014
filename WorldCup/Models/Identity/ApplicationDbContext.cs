d27d19d7cb16ebc18a6b9eca174d8c7d1e58adad
Babis Karypidis <xaralaboskaripidis@gmail.com>
2014-05-14 14:11:05 +0200

    Initialize teams and some matches in code

--- WorldCup/Models/Identity/ApplicationDbContext.cs
+++ WorldCup/Models/Identity/ApplicationDbContext.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Web;
 using Microsoft.AspNet.Identity;
@@ -54,7 +56,76 @@ namespace WorldCup.Models.Identity
         {
             InitializeAdmins();
             InitializeUsers();
+            InitializeTeams(context);
+            InitializeMatches(context);
             base.Seed(context);
+        }
+
+        private void InitializeTeams(ApplicationDbContext context)
+        {
+            var teams = new List<Team>
+            {
+                new Team {Id = "alg", Name = "Algeria", Code = "ALG"},
+                new Team {Id = "arg", Name = "Argentina", Code = "ARG"},
+                new Team {Id = "aus", Name = "Australia", Code = "AUS"},
+                new Team {Id = "bel", Name = "Belgium", Code = "BEL"},
+                new Team {Id = "bih", Name = "Bosnia-Herzegovina", Code = "BIH"},
+                new Team {Id = "bra", Name = "Brazil", Code = "BRA"},
+                new Team {Id = "chi", Name = "Chile", Code = "CHI"},
+                new Team {Id = "civ", Name = "Cote d'Ivore", Code = "CIV"},
+                new Team {Id = "cmr", Name = "Cameroon", Code = "CMR"},
+                new Team {Id = "col", Name = "Colombia", Code = "COL"},
+                new Team {Id = "crc", Name = "Costa Rica", Code = "CRC"},
+                new Team {Id = "cro", Name = "Croatia", Code = "CRO"},
+                new Team {Id = "ecu", Name = "Ecuador", Code = "ECU"},
+                new Team {Id = "eng", Name = "England", Code = "ENG"},
+                new Team {Id = "esp", Name = "Spain", Code = "ESP"},
+                new Team {Id = "fra", Name = "France", Code = "FRA"},
+                new Team {Id = "ger", Name = "Germany", Code = "GER"},
+                new Team {Id = "gha", Name = "Ghana", Code = "Gha"},
+                new Team {Id = "gre", Name = "Greece", Code = "GRE"},
+                new Team {Id = "hon", Name = "Honduras", Code = "HON"},
+                new Team {Id = "irn", Name = "Iran", Code = "IRN"},
+                new Team {Id = "ita", Name = "Italy", Code = "ITA"},
+                new Team {Id = "jpn", Name = "Japan", Code = "JPN"},
+                new Team {Id = "kor", Name = "South Korea", Code = "KOR"},
+                new Team {Id = "mex", Name = "Mexico", Code = "MEX"},
+                new Team {Id = "ned", Name = "Netherlands", Code = "NED"},
+                new Team {Id = "nga", Name = "Nigeria", Code = "NGA"},
+                new Team {Id = "por", Name = "Portugal", Code = "POR"},
+                new Team {Id = "rus", Name = "Russia", Code = "Rus"},
+                new Team {Id = "sui", Name = "Switzerland", Code = "SUI"},
+                new Team {Id = "uru", Name = "Uruguay", Code = "URU"},
+                new Team {Id = "usa", Name = "United States", Code = "USA"}
+            };
+
+            context.Teams.AddRange(teams);
+            context.SaveChanges();
+        }
+
+        private void InitializeMatches(ApplicationDbContext context)
+        {
+            const MatchState state = MatchState.Created;
+            var matches = new List<Match>
+            {
+                new Match{HomeTeamId = "bra", AwayTeamId = "cro", State = state, Stage = MatchStage.GroupA, Date = new DateTime(2014,6,12, 20,0,0)},
+
+                new Match{HomeTeamId = "mex", AwayTeamId = "cmr", State = state, Stage = MatchStage.GroupA, Date = new DateTime(2014,6,13, 16,0,0)},
+                new Match{HomeTeamId = "esp", AwayTeamId = "ned", State = state, Stage = MatchStage.GroupB, Date = new DateTime(2014,6,13, 19,0,0)},
+                new Match{HomeTeamId = "chi", AwayTeamId = "aus", State = state, Stage = MatchStage.GroupB, Date = new DateTime(2014,6,13, 21,0,0)},
+                
+                new Match{HomeTeamId = "col", AwayTeamId = "gre", State = state, Stage = MatchStage.GroupC, Date = new DateTime(2014,6,14, 16,0,0)},
+                new Match{HomeTeamId = "uru", AwayTeamId = "crc", State = state, Stage = MatchStage.GroupD, Date = new DateTime(2014,6,14, 19,0,0)},
+                new Match{HomeTeamId = "eng", AwayTeamId = "ita", State = state, Stage = MatchStage.GroupD, Date = new DateTime(2014,6,14, 22,0,0)},
+
+                new Match{HomeTeamId = "civ", AwayTeamId = "jpn", State = state, Stage = MatchStage.GroupC, Date = new DateTime(2014,6,15, 1,0,0)},
+                new Match{HomeTeamId = "sui", AwayTeamId = "ecu", State = state, Stage = MatchStage.GroupE, Date = new DateTime(2014,6,15, 16,0,0)},
+                new Match{HomeTeamId = "fra", AwayTeamId = "hon", State = state, Stage = MatchStage.GroupE, Date = new DateTime(2014,6,15, 19,0,0)},
+                new Match{HomeTeamId = "arg", AwayTeamId = "bih", State = state, Stage = MatchStage.GroupF, Date = new DateTime(2014,6,15, 22,0,0)}
+            };
+
+            context.Matches.AddRange(matches);
+            context.SaveChanges();
         }
 
         private void InitializeAdmins()

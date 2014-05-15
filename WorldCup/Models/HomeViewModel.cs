da66d6a02bc3f516ee7e3c623541a60f2814830b
Babis Karypidis <xaralaboskaripidis@gmail.com>
2014-05-15 17:59:54 +0200

    Populate user latest results in home screen

--- WorldCup/Models/HomeViewModel.cs
+++ WorldCup/Models/HomeViewModel.cs
@@ -11,5 +11,14 @@ namespace WorldCup.Models
         /// </summary>
         public IList<Match> UpcomingMatches { get; set; }
 
+        public IList<UserMatchModel> UserLatestResults { get; set; }
+
+    }
+
+    public class UserMatchModel
+    {
+        public DateTime Date { get; set; }
+        public string Match { get; set; }
+        public int Points { get; set; }
     }
 }

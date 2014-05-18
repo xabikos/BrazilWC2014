8c6d53f8dde37b5057d272fb65ecafa95513cb7d
Babis Karypidis <xabikos@hotmail.com>
2014-05-19 00:43:01 +0200

    Remove commented code

--- WorldCup/Controllers/BaseController.cs
+++ WorldCup/Controllers/BaseController.cs
@@ -28,26 +28,6 @@ namespace WorldCup.Controllers
         {
             get { return _context ?? (_context = HttpContext.GetOwinContext().Get<ApplicationDbContext>()); }
         }
-        /*
-        protected override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            // http://afana.me/post/aspnet-mvc-internationalization-date-time.aspx
-            // Parse TimeZoneOffset.
-            ViewBag.TimeZoneOffset = TimeSpan.FromMinutes(0); // Default offset (Utc) if cookie is missing.
-            var timeZoneCookie = Request.Cookies["_timeZoneOffset"];
-            if (timeZoneCookie != null)
-            {
-
-                double offsetMinutes;
-                if (double.TryParse(timeZoneCookie.Value, out offsetMinutes))
-                {
-                    // Store in ViewBag. You can use Session, TempData, or anything else.
-                    ViewBag.TimeZoneOffset = TimeSpan.FromMinutes(offsetMinutes);
-                }
-            }
-
-            base.OnActionExecuting(filterContext);
-        }
-        */
+        
     }
 }

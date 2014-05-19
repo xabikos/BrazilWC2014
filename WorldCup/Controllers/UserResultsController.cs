39424720be53494d21d6a6a73324c7a89e0d0dad
rogerlejeune <rpedejong@gmail.com>
2014-05-19 17:23:28 +0200

    Add user results page

---
+++ WorldCup/Controllers/UserResultsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using WorldCup.Models;
+
+namespace WorldCup.Controllers
+{
+    [Authorize]
+    public class UserResultsController : ControllerBase
+    {
+        // GET: UserResults
+        public async Task<ActionResult> Index()
+        {
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+
+            var matches = user.MatchPredictions //.Where(mp => mp.Match.Date < DateTime.UtcNow) TODO uncomment this part
+                .OrderByDescending(mp => mp.Match.Date)
+                .Select(
+                    mp =>
+                        new UserMatchModel
+                        {
+                            Date = mp.Match.Date,
+                            Match = mp.Match.HomeTeam.Name + " vs " + mp.Match.AwayTeam.Name,
+                            Points =
+                                user.MatchPoints.Count(mpoint => mpoint.MatchId == mp.MatchId) != 0
+                                    ? user.MatchPoints.Single(mpoint => mpoint.MatchId == mp.MatchId).Points
+                                    : 0
+                        }).ToList();
+
+            return View(matches);
+        }
+
+    }
+}

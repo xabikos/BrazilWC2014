deb91d3b421d33a91dc3b06021318f78d8bd9c88
Babis Karypidis <xabikos@hotmail.com>
2014-05-22 23:35:24 +0200

    #6 Add separate page for showing match prediction overview after the match has started

---
+++ WorldCup/Models/Predictions/PredictionInfoModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorldCup.Models.Predictions
+{
+    /// <summary>
+    /// View model used to show the info of a match once it has started
+    /// </summary>
+    public class PredictionInfoModel
+    {
+        public string HomeTeamName { get; set; }
+        public string AwayTeamName { get; set; }
+
+        public UserPredictionInfo UserPredictionInfo { get; set; }
+
+        public bool FinalResultsUpdated { get; set; }
+        public string HalfTimeScoreResult { get; set; }
+        public string FullTimeScoreResult { get; set; }
+        public string WinnerResult { get; set; }
+        public int YellowCardsResult { get; set; }
+        public int RedCardsResult { get; set; }
+
+        public IList<UserPredictionInfo> UsersPredictions { get; set; } 
+    }
+
+    public class UserPredictionInfo
+    {
+        public string UserName { get; set; }
+        public string HalfTimeScore { get; set; }
+        public string FullTimeScore { get; set; }
+        public string Winner { get; set; }
+        public int YellowCards { get; set; }
+        public int RedCards { get; set; }
+    }
+}

e6b83f5d9c13c654d81096025317f36f7c18bf53
rogerlejeune <rpedejong@gmail.com>
2014-05-22 14:24:21 +0200

    #15 Add first version of user predictions report

---
+++ WorldCup/Controllers/ReportsController.cs
@@ -0,0 +1,27 @@
+﻿using MvcRazorToPdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WorldCup.Models.Reports;
+using Microsoft.AspNet.Identity;
+
+namespace WorldCup.Controllers
+{
+    [Authorize]
+    public class ReportsController : ControllerBase
+    {
+        // GET: Reports
+        public async Task<ActionResult> UserPredictions()
+        {
+            var model = new UserPredictionsReportsModel();
+
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+
+            return new PdfActionResult(user);
+        }
+
+    }
+}
---
+++ WorldCup/Models/Reports/UserPredictionsReportsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WorldCup.Common.Entities;
+
+namespace WorldCup.Models.Reports
+{
+    public class UserPredictionsReportsModel
+    {
+
+        public string UserName { get; set; }
+        public IEnumerable<MatchPrediction> MatchPredictions { get; set; }
+        public LongRunningPrediction LongRunningPredictions { get; set; }
+
+    }
+}
